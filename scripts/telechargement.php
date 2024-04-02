d521808140cf7d05a25156aea9cbebc5bca5b30a
Amelo32 <147934073+Amelo32@users.noreply.github.com>
2024-04-02 18:21:27 -0400

    Ajout gestion upload et download

--- scripts/telechargement.php
+++ scripts/telechargement.php
@@ -1,12 +1,22 @@
 <?php
+if(empty($_GET['image'])){
+    $imageInfo = getimagesize('images/defaut.jpg');
+    header('Content-Type: '.$imageInfo['mime']);
+    readfile('images/defaut.jpg');
+    exit();
+}
 $nomImage = $_GET['image'];
 $chemin = 'images/'.$nomImage;
+
 
 if(file_exists($chemin)){
     $imageInfo = getimagesize($chemin);
     header('Content-Type: '.$imageInfo['mime']);
     readfile($chemin);
-    exit;
+    exit();
 } else {
-    echo 'Fichier non trouvé';
+    $imageInfo = getimagesize('images/defaut.jpg');
+    header('Content-Type: '.$imageInfo['mime']);
+    readfile('images/defaut.jpg');
+    exit();
 }
--- scripts/televersement.php
+++ scripts/televersement.php
@@ -1,5 +1,5 @@
 <?php
-
+header('content-type: application/json');
 if($_SERVER['REQUEST_METHOD'] == 'POST') {
     if(isset($_FILES['image'])){
         $erreurs = [];
@@ -7,8 +7,36 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
         $cheminTemporaire = $image['tmp_name'];
         $nomFichier = $image['name'];
         
+        
+        // Vérifier si le fichier est une image
+        $extensionsValides = ['jpg', 'jpeg', 'png', 'gif'];
+        $extensionFichier = strtolower(pathinfo($nomFichier, PATHINFO_EXTENSION));
 
-    
+        if(!in_array($extensionFichier, $extensionsValides)){
+            $erreurs[] = 'Le fichier doit être une image (JPG, JPEG, PNG ou GIF)';
+        }
+
+        if(empty($erreurs)){
+            $chemin = 'images/'.$nomFichier;
+            move_uploaded_file($cheminTemporaire, $chemin);
+            
+            $nouveauNom = 'images/' . mt_rand('100000', '999999') . '.' . $extensionFichier;
+            while(file_exists($nouveauNom)){
+                $nouveauNom = mt_rand('100000', '999999') . '.' . $extensionFichier;
+            }
+            if(rename($chemin,'images/' . $nouveauNom)){
+                echo json_encode(['chemin' => $nouveauNom]);
+                exit();
+            }
+             else {
+                $erreurs[] = 'Erreur lors du renommage du fichier'; 
+                echo json_encode(['erreurs' => $erreurs]);
+            }
+            exit();
+        } else {
+            echo json_encode(['erreurs' => $erreurs]);
+            exit();
+        }
     }
 
 }
