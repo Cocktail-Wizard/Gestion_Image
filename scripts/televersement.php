b80d3fee2c6e35f2e9f15e5b12c88398dd3ed64c
Amelo32 <147934073+Amelo32@users.noreply.github.com>
2024-04-02 18:16:00 -0400

    Get image

--- scripts/televersement.php
+++ scripts/televersement.php
@@ -4,6 +4,8 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
     if(isset($_FILES['image'])){
         $erreurs = [];
         $image = $_FILES['image'];
+        $cheminTemporaire = $image['tmp_name'];
+        $nomFichier = $image['name'];
         
 
     
